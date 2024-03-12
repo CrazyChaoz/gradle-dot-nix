fb43797a4cc0511830791a94f46f17bfcb7d354b
Stefan Kempinger <19308955+CrazyChaoz@users.noreply.github.com>
2024-03-12 22:31:38 +0100

    Create rename-module.py

---
+++ rename-module.py
@@ -0,0 +1,21 @@
+import sys
+import json
+import os
+
+module_file = sys.argv[1]
+
+with open(module_file, 'r') as json_file:
+        module_data = json.load(json_file)
+        renaming_aliases = {}
+        for variant in module_data.get('variants', []):
+            for file in variant.get('files', []):
+                renaming_aliases[file['name']] = file['url']
+
+        artifact_name = sys.argv[2]
+        if renaming_aliases.get(artifact_name):
+            artifact_name = renaming_aliases.get(artifact_name)
+
+        artifact_name = os.path.join(sys.argv[3], artifact_name)
+
+        with open(os.path.join(sys.argv[4]), 'w') as output_file:
+            output_file.write(artifact_name)
