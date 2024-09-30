2a58146840f1197215c6ec461098237e581b4387
Stefan Kempinger <19308955+CrazyChaoz@users.noreply.github.com>
2024-09-30 15:25:22 +0200

    maybe fix: attempt to call something which is not a function but a set: { success = true; value = «primop map»; }

--- default.nix
+++ default.nix
@@ -173,21 +173,21 @@ let
       pom-file-derivation unique-dependency
     else if unique-dependency.has_module_file == "true" then
       let
-        tryEval-artifact = builtins.elemAt (builtins.filter (elem: elem.success == true) (
-          builtins.tryEval map (
+        tryEval-artifact = builtins.elemAt (builtins.filter (elem: elem.success == true)
+          builtins.tryEval (map (
             sha256-module:
             map (
               sha256-deps: (full-artifact-derivation unique-dependency sha256-deps sha256-module)
             ) unique-dependency.sha_256
-          ) unique-dependency.module_file.sha_256
-        )) 0;
+          ) unique-dependency.module_file.sha_256)
+        ) 0;
       in
       tryEval-artifact.value
     else
       let
-        tryEval-artifact = builtins.elemAt (builtins.filter (elem: elem.success == true) (
-          builtins.tryEval map (sha256: (module-file-derivation unique-dependency sha256)) unique-dependency.sha_256
-        )) 0;
+        tryEval-artifact = builtins.elemAt (builtins.filter (elem: elem.success == true)
+          builtins.tryEval (map (sha256: (module-file-derivation unique-dependency sha256)) unique-dependency.sha_256)
+        ) 0;
       in
       tryEval-artifact.value;
 
